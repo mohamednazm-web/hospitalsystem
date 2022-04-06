32a6ff92f8f4fdfed22e8cbcad3545da8eea5419
mohamednazm-web <mhamadnazm53@gmail.com>
2022-04-06 16:11:49 +0300

    first commit

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -77,14 +77,11 @@ check_login();
 										</div>
 									</div>
 								</div>
-								
-
-<div class="col-sm-4">
+								<div class="col-sm-4">
 									<div class="panel panel-white no-radius text-center">
 										<div class="panel-body">
 											<span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-smile-o fa-stack-1x fa-inverse"></i> </span>
-											<h2 class="StepTitle">Manage Patients</h2>
-											
+											<h2 class="StepTitle">Manage Patients</h2>	
 											<p class="links cl-effect-1">
 												<a href="manage-patient.php">
 													<?php $result = mysqli_query($con,"SELECT * FROM tblpatient ");
